1b67dc92183a1878e814c0e22ea293c34330421b
Camilo <pedrazacamilo630@gmail.com>
2024-08-31 13:47:47 -0500

    autenticacion finalizado

--- Backend/ribuzz-project/src/Auth/google.strategy.ts
+++ Backend/ribuzz-project/src/Auth/google.strategy.ts
@@ -12,7 +12,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
         super({
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:5000/auth/google/callback',
+            callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:3000/auth/google/callback',
             scope: ['email', 'profile'],
         });
         
