5304a271270a790f31410efbc0206185baef6be2
FT51Grupo02 <pfgrupo2ft51@gmail.com>
2024-09-05 11:56:19 -0500

    Habilitación puerto 3005

--- Backend/ribuzz-project/src/Auth/auth.controller.ts
+++ Backend/ribuzz-project/src/Auth/auth.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Body, Controller, Get, Post, Req, Res, UseGuards } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthDto } from "./Dto/auth.dto";
