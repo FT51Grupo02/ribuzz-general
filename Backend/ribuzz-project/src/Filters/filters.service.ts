20d99893a691baebbe4a20a30ee11fb239d6850b
Luciano Emerí <162066631+LucianoEmeri@users.noreply.github.com>
2024-09-16 12:27:00 -0300

    Filtros 1

--- Backend/ribuzz-project/src/Filters/filters.service.ts
+++ Backend/ribuzz-project/src/Filters/filters.service.ts
@@ -5,7 +5,6 @@ import { Products } from '../Entidades/products.entity';
 import { Services } from 'src/Entidades/services.entity';
 import { Events } from 'src/Entidades/events.entity';
 import { FilterDto } from './Dto/filters.dto';
-import { DateFormatService } from 'src/DateFormat/dateformat.service';
 
 @Injectable()
 export class FilterService {
@@ -15,36 +14,26 @@ export class FilterService {
     @InjectRepository(Services)
     private readonly serviceRepository: Repository<Services>,
     @InjectRepository(Events)
-    private readonly eventRepository: Repository<Events>,
-    private readonly dataFormatService: DateFormatService
+    private readonly eventRepository: Repository<Events>
   ) {}
 
   async searchProducts(dto: FilterDto): Promise<Products[]> {
     try {
       const arrayProduct = this.productRepository.createQueryBuilder('product')
-        .leftJoinAndSelect('product.orders', 'order')
-        .leftJoinAndSelect('product.categories', 'category');
+        .leftJoinAndSelect('product.categories', 'category')
+        .leftJoin('DETAILS_ORDER_PRODUCTS', 'details', 'details.productsId = product.id')
+        .leftJoin('details.order', 'order');
 
-      const textFilters = {
-        ...(dto.name && { name: dto.name }),
-        ...(dto.populate && { populate: dto.populate }),
-      };
-
-      const numberFilters = {
-        ...(dto.price && { price: dto.price }),
-        ...(dto.rating && { rating: dto.rating }),
-      };
-
-      if (Object.keys(textFilters).length > 0) {
-        Object.entries(textFilters).forEach(([key, value]) => {
-          arrayProduct.andWhere(`product.${key} ILIKE :${key}`, { [`${key}`]: `%${value}%` });
-        });
+      if (dto.name) {
+        arrayProduct.andWhere('product.name ILIKE :name', { name: `%${dto.name}%` });
       }
 
-      if (Object.keys(numberFilters).length > 0) {
-        Object.entries(numberFilters).forEach(([key, value]) => {
-          arrayProduct.andWhere(`product.${key} = :${key}`, { [`${key}`]: value });
-        });
+      if (dto.price) {
+        arrayProduct.andWhere('product.price = :price', { price: dto.price });
+      }
+
+      if (dto.rating) {
+        arrayProduct.andWhere('product.rating = :rating', { rating: dto.rating });
       }
 
       if (dto.publicateDate) {
@@ -52,25 +41,21 @@ export class FilterService {
       }
 
       if (dto.categories) {
-        const categoriesArray = Array.isArray(dto.categories) ? dto.categories : [dto.categories];
-        arrayProduct.andWhere('category.name IN (:...categories)', { categories: categoriesArray });
+        arrayProduct.andWhere('category.name = :category', { category: dto.categories });
       }
 
-      if (dto.orderPrice && (dto.orderPrice.toLowerCase() === 'asc' || dto.orderPrice.toLowerCase() === 'desc')) {
-        const order: 'ASC' | 'DESC' = dto.orderPrice.toLowerCase() === 'asc' ? 'ASC' : 'DESC';
-        arrayProduct.addOrderBy('product.price', order);
+      if (dto.orderPrice) {
+        arrayProduct.addOrderBy('product.price', dto.orderPrice);
       }
 
-      if (dto.orderRating && (dto.orderRating.toLowerCase() === 'asc' || dto.orderRating.toLowerCase() === 'desc')) {
-        const order: 'ASC' | 'DESC' = dto.orderRating.toLowerCase() === 'asc' ? 'ASC' : 'DESC';
-        arrayProduct.addOrderBy('product.rating', order);
+      if (dto.orderRating) {
+        arrayProduct.addOrderBy('product.rating', dto.orderRating);
       }
 
-      if (dto.orderPopularity && (dto.orderPopularity.toLowerCase() === 'asc' || dto.orderPopularity.toLowerCase() === 'desc')) {
-        const order: 'ASC' | 'DESC' = dto.orderPopularity.toLowerCase() === 'asc' ? 'ASC' : 'DESC';
-        arrayProduct.addSelect('COUNT(order.id)', 'orderCount')
-                    .groupBy('product.id')
-                    .addOrderBy('orderCount', order);
+      if (dto.orderPopularity) {
+        arrayProduct.addSelect('COUNT(DISTINCT order.id)', 'orderCount')
+          .groupBy('product.id')
+          .addOrderBy('orderCount', dto.orderPopularity);
       }
 
       return await arrayProduct.getMany();
@@ -82,115 +67,102 @@ export class FilterService {
   async searchServices(dto: FilterDto): Promise<Services[]> {
     try {
       const arrayService = this.serviceRepository.createQueryBuilder('service')
-        .leftJoinAndSelect('service.orders', 'order')
-        .leftJoinAndSelect('service.categories', 'category');
+        .leftJoinAndSelect('service.categories', 'category')
+        .leftJoin('DETAILS_ORDER_SERVICE', 'details', 'details.serviceId = service.id')
+        .leftJoin('details.order', 'order');
 
-      const textFilters = {
-        ...(dto.name && { name: dto.name }),
-        ...(dto.duration && { duration: dto.duration }),
-        ...(dto.location && { location: dto.location })
-      };
+      if (dto.name) {
+        arrayService.andWhere('service.name ILIKE :name', { name: `%${dto.name}%` });
+      }
 
-      const numberFilters = {
-        ...(dto.price && { price: dto.price }),
-        ...(dto.rating && { rating: dto.rating }),
-      };
+      if (dto.price) {
+        arrayService.andWhere('service.price = :price', { price: dto.price });
+      }
 
-      Object.entries(textFilters).forEach(([key, value]) => {
-        arrayService.andWhere(`service.${key} ILIKE :${key}`, { [`${key}`]: `%${value}%` });
-      });
-
-      Object.entries(numberFilters).forEach(([key, value]) => {
-        arrayService.andWhere(`service.${key} = :${key}`, { [`${key}`]: value });
-      });
+      if (dto.rating) {
+        arrayService.andWhere('service.rating = :rating', { rating: dto.rating });
+      }
 
       if (dto.publicateDate) {
         arrayService.andWhere('service.publicateDate = :publicateDate', { publicateDate: dto.publicateDate });
       }
 
+      if (dto.location) {
+        arrayService.andWhere("service.providerInfo->>'location' ILIKE :location", { location: `%${dto.location}%` });
+      }
+
       if (dto.categories) {
-        const categoriesArray = Array.isArray(dto.categories) ? dto.categories : [dto.categories];
-        arrayService.andWhere('category.name IN (:...categories)', { categories: categoriesArray });
+        arrayService.andWhere('category.name = :category', { category: dto.categories });
       }
 
-      if (dto.orderPrice && (dto.orderPrice.toLowerCase() === 'asc' || dto.orderPrice.toLowerCase() === 'desc')) {
-        const order: 'ASC' | 'DESC' = dto.orderPrice.toLowerCase() === 'asc' ? 'ASC' : 'DESC';
-        arrayService.addOrderBy('service.price', order);
+      if (dto.orderPrice) {
+        arrayService.addOrderBy('service.price', dto.orderPrice);
       }
 
-      if (dto.orderRating && (dto.orderRating.toLowerCase() === 'asc' || dto.orderRating.toLowerCase() === 'desc')) {
-        const order: 'ASC' | 'DESC' = dto.orderRating.toLowerCase() === 'asc' ? 'ASC' : 'DESC';
-        arrayService.addOrderBy('service.rating', order);
+      if (dto.orderRating) {
+        arrayService.addOrderBy('service.rating', dto.orderRating);
       }
 
-      if (dto.orderPopularity && (dto.orderPopularity.toLowerCase() === 'asc' || dto.orderPopularity.toLowerCase() === 'desc')) {
-        const order: 'ASC' | 'DESC' = dto.orderPopularity.toLowerCase() === 'asc' ? 'ASC' : 'DESC';
-        arrayService.addSelect('COUNT(order.id)', 'orderCount')
-                    .groupBy('service.id')
-                    .addOrderBy('orderCount', order);
+      if (dto.orderPopularity) {
+        arrayService.addSelect('COUNT(DISTINCT order.id)', 'orderCount')
+          .groupBy('service.id')
+          .addOrderBy('orderCount', dto.orderPopularity);
       }
 
       return await arrayService.getMany();
     } catch (error) {
-      console.error('Error al encontrar el servicio:', error);
-      throw new InternalServerErrorException('Error al encontrar el servicio: ' + error);
+      throw new InternalServerErrorException("Error al encontrar el servicio: " + error);
     }
   }
 
   async searchEvents(dto: FilterDto): Promise<Events[]> {
     try {
       const arrayEvent = this.eventRepository.createQueryBuilder('event')
-        .leftJoinAndSelect('event.orders', 'order')
-        .leftJoinAndSelect('event.categories', 'category');
+        .leftJoinAndSelect('event.categories', 'category')
+        .leftJoin('DETAILS_ORDER_EVENTS', 'details', 'details.eventsId = event.id')
+        .leftJoin('details.order', 'order');
 
-      const textFilters = {
-        ...(dto.name && { name: dto.name }),
-        ...(dto.location && { location: dto.location })
-      };
+      if (dto.name) {
+        arrayEvent.andWhere('event.name ILIKE :name', { name: `%${dto.name}%` });
+      }
 
-      const numberFilters = {
-        ...(dto.price && { price: dto.price }),
-        ...(dto.rating && { rating: dto.rating }),
-      };
+      if (dto.price) {
+        arrayEvent.andWhere('event.price = :price', { price: dto.price });
+      }
 
-      Object.entries(textFilters).forEach(([key, value]) => {
-        arrayEvent.andWhere(`event.${key} ILIKE :${key}`, { [`${key}`]: `%${value}%` });
-      });
-
-      Object.entries(numberFilters).forEach(([key, value]) => {
-        arrayEvent.andWhere(`event.${key} = :${key}`, { [`${key}`]: value });
-      });
+      if (dto.rating) {
+        arrayEvent.andWhere('event.rating = :rating', { rating: dto.rating });
+      }
 
       if (dto.publicateDate) {
         arrayEvent.andWhere('event.publicateDate = :publicateDate', { publicateDate: dto.publicateDate });
       }
 
+      if (dto.location) {
+        arrayEvent.andWhere('event.location ILIKE :location', { location: `%${dto.location}%` });
+      }
+
       if (dto.categories) {
-        const categoriesArray = Array.isArray(dto.categories) ? dto.categories : [dto.categories];
-        arrayEvent.andWhere('category.name IN (:...categories)', { categories: categoriesArray });
+        arrayEvent.andWhere('category.name = :category', { category: dto.categories });
       }
 
-      if (dto.orderPrice && (dto.orderPrice.toLowerCase() === 'asc' || dto.orderPrice.toLowerCase() === 'desc')) {
-        const order: 'ASC' | 'DESC' = dto.orderPrice.toLowerCase() === 'asc' ? 'ASC' : 'DESC';
-        arrayEvent.addOrderBy('event.price', order);
+      if (dto.orderPrice) {
+        arrayEvent.addOrderBy('event.price', dto.orderPrice);
       }
 
-      if (dto.orderRating && (dto.orderRating.toLowerCase() === 'asc' || dto.orderRating.toLowerCase() === 'desc')) {
-        const order: 'ASC' | 'DESC' = dto.orderRating.toLowerCase() === 'asc' ? 'ASC' : 'DESC';
-        arrayEvent.addOrderBy('event.rating', order);
+      if (dto.orderRating) {
+        arrayEvent.addOrderBy('event.rating', dto.orderRating);
       }
 
-      if (dto.orderPopularity && (dto.orderPopularity.toLowerCase() === 'asc' || dto.orderPopularity.toLowerCase() === 'desc')) {
-        const order: 'ASC' | 'DESC' = dto.orderPopularity.toLowerCase() === 'asc' ? 'ASC' : 'DESC';
-        arrayEvent.addSelect('COUNT(order.id)', 'orderCount')
-                  .groupBy('event.id')
-                  .addOrderBy('orderCount', order);
+      if (dto.orderPopularity) {
+        arrayEvent.addSelect('COUNT(DISTINCT order.id)', 'orderCount')
+          .groupBy('event.id')
+          .addOrderBy('orderCount', dto.orderPopularity);
       }
 
       return await arrayEvent.getMany();
     } catch (error) {
-      console.error('Error al encontrar el evento:', error);
-      throw new InternalServerErrorException('Error al encontrar el evento: ' + error);
+      throw new InternalServerErrorException("Error al encontrar el evento: " + error);
     }
   }
 }
